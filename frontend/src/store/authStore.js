2b0c86e49e51d95fa3cd34c7dc12af9b80b669a3
asthasingh0660 <asthasingh0660@gmail.com>
2024-09-26 21:03:34 +0530

    URL updated in authStore.js

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";   //For global state management
 import axios from "axios";
 
-const API_URL = import.meta.env.MODE === "production" ? "http://localhost:5000/api/auth" : "/api/auth";
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
 
 axios.defaults.withCredentials = true;
 
